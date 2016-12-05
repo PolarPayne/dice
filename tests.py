1aa6bd8d667c633dbac356ff0d4b67418e502969
Max Remming <maxremming@gmail.com>
2016-12-05 12:17:42 +0200

    Started tests

---
+++ tests.py
@@ -0,0 +1,21 @@
+def test():
+    def f(c, mi, ma):
+        float(mi), float(ma)
+        assert mi != ma
+        print(c, mi, ma)
+
+    def g(c, r):
+        float(r)
+        print(c, r)
+
+    with open("tests.txt", "r") as file:
+        file.readline()
+        for line in file:
+            print(line)
+            line = line.strip().split()
+            if len(line) == 2:
+                g(line[0], line[1])
+            elif len(line) == 3:
+                f(line[0], line[1], line[2])
+            else:
+                raise Exception("Badly formatted tests.txt file.")
