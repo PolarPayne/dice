22357de3abe103221698e239702554d4c8ec5a10
Maximilian Remming <maxremming@gmail.com>
2016-12-29 21:40:29 +0200

    Large part of main functionality

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        code = request.data.decode("utf-8")
+        return json.jsonify({
+            "code": code
+        })
+
+    return render_template("index.html", code="")
+
 
 if __name__ == "__main__":
     app.run()
--- static/code.js
+++ static/code.js
@@ -1,10 +1,37 @@
-document.getElementById("code").addEventListener("keypress", execute);
+var codeElement = document.getElementById("code");
+codeElement.addEventListener("keypress", execute);
+codeElement.focus();
+
+function toggleTipsHints() {
+    console.log("toggle tips and hints section");
+    return false;
+}
 
 function execute(e) {
     if (e !== undefined && !e.ctrlKey)
         return;  
-    var code = document.getElementById("code").value;
+    var code = codeElement.value;
     console.log(code);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/", true);
+    xhr.setRequestHeader("Content-type", "text/plain");
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+            if (xhr.getResponseHeader("Content-type") !== "application/json" || xhr.responseText === null) {
+                return;  // failure
+            }
+            var data = JSON.parse(xhr.responseText);
+            console.log(data);
+        }
+    }
+
+    // xhr.send("foo=bar&lorem=ipsum"); 
+    xhr.send(code); 
+    // xhr.send(new Blob()); 
+    // xhr.send(new Int8Array()); 
+    // xhr.send({ form: 'data' }); 
+    // xhr.send(document);
 
     return false;
 }
