a18ff8b9ee793ca4b736d69e36ee8b433135fdf4
Maximilian Remming <maxremming@gmail.com>
2016-12-31 11:52:03 +0200

    Only use minified code if not in debug mode

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import dice
 app = Flask(__name__)
 
 
+@app.context_processor
+def inject_debug():
+    return { "debug": app.debug }
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -14,3 +19,4 @@ def index():
 
 if __name__ == "__main__":
     app.run()
+    print(app.debug)
